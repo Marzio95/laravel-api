c449331056052766af58514ce6be7c391ae4ec8d
Marzio95 <marziodellarocca14@gmail.com>
2022-05-26 15:47:17 +0200

    implemento vue e creo il controllerPost in api e lo collego alla url api/posts passando i dati Axios

---
+++ resources/js/components/ContainerPost.vue
@@ -0,0 +1,39 @@
+<template>
+
+<div class="row">
+    <div class="col-4 ">
+        <div class="card" style="width: 18rem;">
+        <!-- <img class="card-img-top" src=".../100px180/" alt="Card image cap"> -->
+            <div class="card-body">
+                <h5 class="card-title">{{ }}</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+name: 'ContainerPost',
+data() {
+    return{
+        posts:[],
+
+    }
+},
+created () {
+    Axios.get('http://localhost:8080/api/post')
+            .then(res => {
+                console.log(res.data)
+                this.posts = res.data.response.data;
+            })
+}
+}
+</script>
+
+<style>
+
+</style>
